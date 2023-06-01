66b0936ad233c115ffb1fa4e3fdab86489f40d95
Corybyte <rzd200213@gmail.com>
2023-06-01 15:46:52 +0800

    123

---
+++ init.lua
@@ -0,0 +1,18 @@
+-- 系统设置
+require('options')
+
+-- 键盘映射
+
+
+-- 插件管理
+require('plugins')
+
+
+vim.g.defx_icons_enable_syntax_highlight = 1
+vim.g.defx_icons_column_length = 2
+vim.g.defx_icons_directory_icon = ''
+vim.g.defx_icons_root_opened_tree_icon = 'ﱮ'
+vim.g.defx_icons_nvim_web_devicons = 1
+vim.g.defx_columns = 'indent:git:icons:filename:type:size:time'
+vim.g.defx_sort_order = 'time'
+
---
+++ lua/plugins.lua
@@ -0,0 +1,5 @@
+-- 配置插件
+return require('packer').startup(function()
+  -- 插件列表
+  use 'preservim/nerdtree'
+end)
